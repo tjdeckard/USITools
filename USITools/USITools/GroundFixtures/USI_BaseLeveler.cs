9209b6d4f4f60105fd165aba7507c6012a0b73b4
tjdeckard <tjdeckard@users.noreply.github.com>
2021-03-29 20:53:04 -0400

    Add new ground anchor class

---
+++ USITools/USITools/GroundFixtures/USI_BaseLeveler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace USITools
+{
+    public class USI_BaseLeveler : PartModule
+    {
+        private const string PAW_GROUP_NAME = "usi-base-leveler";
+        private const string PAW_GROUP_DISPLAY_NAME = "USI Leveler";
+
+        private USI_GroundAnchorModule _groundAnchor;
+        private Transform _groundContactTransform;
+        private string _noGroundAnchorMessage
+            = "Vessel must have a part with ground anchor to activate leveler";
+        private string _notLandedMessage = "Vessel must be landed to activate leveler";
+
+        #region KSP fields
+        private string GroundContactTransformName;
+        #endregion
+
+        #region KSP actions and events
+        [KSPAction(guiName = "Level now")]
+        public void LevelAction(KSPActionParam param)
+        {
+            Level();
+        }
+
+        [KSPEvent(
+            guiName = "Level now",
+            guiActive = true,
+            guiActiveEditor = false,
+            guiActiveUnfocused = true,
+            unfocusedRange = 5.0f,
+            groupName = PAW_GROUP_NAME,
+            groupDisplayName = PAW_GROUP_DISPLAY_NAME)]
+        public void LevelEvent()
+        {
+            Level();
+        }
+        #endregion
+
+        private void Level()
+        {
+            if (_groundAnchor == null)
+            {
+                ScreenMessages.PostScreenMessage(
+                    _noGroundAnchorMessage,
+                    5.0f,
+                    ScreenMessageStyle.UPPER_CENTER);
+            }
+            else if (!vessel.LandedOrSplashed)
+            {
+                ScreenMessages.PostScreenMessage(
+                    _notLandedMessage,
+                    5.0f,
+                    ScreenMessageStyle.UPPER_CENTER);
+            }
+            else
+            {
+                _groundAnchor.DisableAllEvent();
+
+            }
+        }
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+
+            if (!string.IsNullOrEmpty(GroundContactTransformName))
+            {
+                var groundContactGO = GameObject.Find(GroundContactTransformName);
+                _groundContactTransform = groundContactGO.transform ?? part.transform;
+            }
+            else
+            {
+                _groundContactTransform = part.transform;
+            }
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                _groundAnchor = vessel.FindPartModulesImplementing<USI_GroundAnchorModule>()
+                    .FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ USITools/USITools/GroundFixtures/USI_GroundAnchorModule.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+namespace USITools
+{
+    public class USI_GroundAnchorModule : PartModule
+    {
+        private const string PAW_GROUP_NAME = "usi-ground-anchor";
+        private const string PAW_GROUP_DISPLAY_NAME = "USI Ground Anchor";
+
+        private FixedJoint _anchorJoint;
+        private string _notLandedMessage = "Vessel must be landed to enable ground anchor";
+
+        #region KSP fields
+        [KSPField(isPersistant = true)]
+        private bool IsAnchored;
+        #endregion
+
+        #region KSP actions and events
+#pragma warning disable IDE0060 // Remove unused parameter
+
+        [KSPAction(guiName = "Disable ground anchor")]
+        public void DisableAnchorAction(KSPActionParam param)
+        {
+            ToggleAnchor(false);
+        }
+
+        [KSPAction(guiName = "Enable ground anchor")]
+        public void EnableAnchorAction(KSPActionParam param)
+        {
+            ToggleAnchor(true);
+        }
+
+        [KSPAction(guiName = "Toggle ground anchor")]
+        public void ToggleAnchorAction(KSPActionParam param)
+        {
+            ToggleAnchor(!IsAnchored);
+        }
+
+        [KSPEvent(
+            guiName = "Ground anchor",
+            guiActive = true,
+            guiActiveEditor = false,
+            guiActiveUnfocused = true,
+            unfocusedRange = 5.0f,
+            groupName = PAW_GROUP_NAME,
+            groupDisplayName = PAW_GROUP_DISPLAY_NAME)]
+        public void ToggleAnchorEvent()
+        {
+            ToggleAnchor(!IsAnchored);
+        }
+
+        [KSPEvent(
+            guiName = "Enable all ground anchors",
+            guiActive = true,
+            guiActiveEditor = false,
+            guiActiveUnfocused = true,
+            unfocusedRange = 5.0f,
+            groupName = PAW_GROUP_NAME,
+            groupDisplayName = PAW_GROUP_DISPLAY_NAME)]
+        public void EnableAllEvent()
+        {
+            ToggleAllAnchors(true);
+        }
+
+        [KSPEvent(
+            guiName = "Disable all ground anchors",
+            guiActive = true,
+            guiActiveEditor = false,
+            guiActiveUnfocused = true,
+            unfocusedRange = 5.0f,
+            groupName = PAW_GROUP_NAME,
+            groupDisplayName = PAW_GROUP_DISPLAY_NAME)]
+        public void DisableAllEvent()
+        {
+            ToggleAllAnchors(false);
+        }
+
+#pragma warning restore IDE0060 // Remove unused parameter
+        #endregion
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                ToggleJoint(IsAnchored);
+                UpdatePAW(IsAnchored);
+            }
+        }
+
+        private void ToggleAllAnchors(bool isActive)
+        {
+            ToggleAnchor(isActive);
+
+            var anchors = vessel.FindPartModulesImplementing<USI_GroundAnchorModule>();
+            for (int i = 0; i < anchors.Count; i++)
+            {
+                anchors[i].ToggleAnchor(isActive);
+            }
+        }
+
+        public void ToggleAnchor(bool isActive)
+        {
+            if (IsAnchored != isActive)
+            {
+                ToggleJoint(isActive);
+                UpdatePAW(isActive);
+                IsAnchored = isActive;
+            }
+        }
+
+        private void ToggleJoint(bool isActive)
+        {
+            if (isActive && _anchorJoint == null)
+            {
+                if (!vessel.LandedOrSplashed)
+                {
+                    ScreenMessages.PostScreenMessage(
+                        _notLandedMessage,
+                        5.0f,
+                        ScreenMessageStyle.UPPER_CENTER);
+                }
+                else
+                {
+                    _anchorJoint = gameObject.AddComponent<FixedJoint>();
+                    _anchorJoint.enableCollision = true;
+                    _anchorJoint.autoConfigureConnectedAnchor = true;
+                    _anchorJoint.breakForce = float.PositiveInfinity;
+                    _anchorJoint.breakTorque = float.PositiveInfinity;
+                }
+            }
+            else if (!isActive && _anchorJoint != null)
+            {
+                Destroy(_anchorJoint);
+                _anchorJoint = null;
+            }
+        }
+
+        private void UpdatePAW(bool isActive)
+        {
+            Events[nameof(ToggleAnchorEvent)].guiName
+                = $"Ground anchor: {(isActive ? "On" : "Off")}";
+            MonoUtilities.RefreshPartContextWindow(part);
+        }
+    }
+}
